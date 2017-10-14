28a0de0d9dc2145200688e7e9d3c5742a72ebd86
mjjimenez <mjjimenez87@gmail.com>
2017-10-14 20:03:24 +0800

    Initial commit

---
+++ Amaysim-Exam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amaysim-Exam
+//
+//  Created by Mark Jeremiah Jimenez on 14/10/2017.
+//  Copyright © 2017 Mark Jeremiah Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
